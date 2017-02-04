37a2f98f866a4dd40436dda031abb09422d0b5c3
sunlei213 <sunleity@163.com>
2017-02-04 22:49:24 +0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # py35
 上交所fast转dbf程序（pytho版）
+修改一下
---
+++ test.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@version: python3.5
+@author:  sunlei
+@license: Apache Licence
+@contact: 12166056@qq.com
+@site: git
+@software: PyCharm Community Edition
+@file: dbfclass.py
+@time: 2017/2/2 13:55
+"""
+
+sl1 = u'你好世界'
+print(sl1)
+sl2 = 4
+print('{0}.{1}'.format(sl2, sl1))
+sl1 = 4
