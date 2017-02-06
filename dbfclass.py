9d463235e4edeb8daa5670c9b1050097e5c02ba7
sunlei213 <sunleity@163.com>
2017-02-06 09:22:43 +0800

    上交所fast转dbf（Python版）

--- dbfclass.py
+++ dbfclass.py
@@ -16,7 +16,7 @@ import struct
 import datetime
 import io
 
-FIELD = col.namedtuple('Field', "name type lenth dec")
+FIELD = col.namedtuple('Field', "name type length dec")
 HEAD_FORMAT = '<BBBBLHH20x'
 FIELD_FORMAT = '<11sc4xBB14x'
 
@@ -30,19 +30,19 @@ def recode_to_stream(f, fields, recodes, is_sse=True):
                 del_flag = b'\x42' if recode[i] else b'\x32'
                 f.write(del_flag)
                 continue
-            if len(recode[i]) > field.lenth:
+            if len(recode[i]) > field.length:
                 raise "记录{3:d}长度：{1:d}大于字段{0}设定长度：{2:d}，内容：{4}".format(
-                    field.name, len(recode[i]), field.lenth, reno, recode[i])
+                    field.name, len(recode[i]), field.length, reno, recode[i])
             if reno == 1 and i == 5 and is_sse:
-                value = str(recode[i])[:field.lenth].ljust(field.lenth, ' ').encode()
+                value = str(recode[i])[:field.length].ljust(field.length, ' ').encode()
             elif field.type == 'N' or field.type == 'F':
-                value = str(recode[i]).rjust(field.lenth, ' ').encode()
+                value = str(recode[i]).rjust(field.length, ' ').encode()
             elif field.type == 'D':
                 value = recode[i].strftime('%Y%m%d').encode()
             elif field.type == 'L':
                 value = str(recode[i])[0].upper().encode()
             else:
-                value = str(recode[i])[:field.lenth].ljust(field.lenth, ' ').encode()
+                value = str(recode[i])[:field.length].ljust(field.length, ' ').encode()
             f.write(value)
 
 
@@ -54,7 +54,7 @@ def head_to_stream(f, fields, recnum):
     yr, mon, day = [now.year - 1900, now.month, now.day]
     numfields = len(fields)
     lenheader = numfields * 32 + 33
-    lenrecord = sum(field.lenth for field in fields)
+    lenrecord = sum(field.length for field in fields)
     hdr = struct.pack(HEAD_FORMAT, ver, yr, mon, day, recnum, lenheader, lenrecord)
     f.write(hdr)
 
@@ -63,7 +63,7 @@ def head_to_stream(f, fields, recnum):
         if field.name == "del_flag":
             continue
         name = field.name.ljust(11, '\x00').encode()
-        fld = struct.pack(FIELD_FORMAT, name, field.type[0].encode(), field.lenth, field.dec)
+        fld = struct.pack(FIELD_FORMAT, name, field.type[0].encode(), field.length, field.dec)
         f.write(fld)
 
     # 结束文件头
@@ -74,7 +74,7 @@ class DbfSseWriter(object):
     def __init__(self, field_list=None):
         self.stream = io.BytesIO()
         if field_list:
-            self.fields = [FIELD(name, Type, lenth, dec) for [name, Type, lenth, dec] in field_list]
+            self.fields = [FIELD(name, Type, length, dec) for [name, Type, length, dec] in field_list]
         if self.fields:
             self.fields.insert(0, FIELD('del_flag', 'C', 1, 0))
         else:
@@ -91,9 +91,27 @@ class DbfSseWriter(object):
     def stream_to_file(self, file_stream=None):
         if not file_stream:
             return False
-        self.stream.seek(0,2)
+        self.stream.seek(0, 2)
         stream_len = len(self.stream.tell())
         file_stream.truncate(stream_len)
         file_stream.seek(0)
         file_stream.write(self.stream.getvalue())
         return True
+
+    @property
+    def fields(self):
+        return self.fields
+
+    @fields.setter
+    def fields(self, value):
+        """
+
+        :type value: list of field
+        filed sample FIELD(name,type,length,dec)
+        """
+        if len(value) == 0:
+            return
+        elif value[0].name != 'del_flag':
+            self.fields = [FIELD('del_flag', 'C', 1, 0)] + value
+        else:
+            self.fields = value
