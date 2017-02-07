05e80cde601447251114cf8cf7855d361c4222f0
sunlei213 <sunleity@163.com>
2017-02-07 18:17:34 +0800

    上交所fast转dbf（Python版）

--- dbfclass.py
+++ dbfclass.py
@@ -16,6 +16,7 @@ import struct
 import datetime
 import io
 
+
 FIELD = col.namedtuple('Field', "name type length dec")
 HEAD_FORMAT = '<BBBBLHH20x'
 FIELD_FORMAT = '<11sc4xBB14x'
@@ -24,25 +25,39 @@ FIELD_FORMAT = '<11sc4xBB14x'
 def recode_to_stream(f, fields, recodes, is_sse=True):
     """f is BytesIO, fields is list of field and first value is del_flag,
        recodes is a list of recode"""
+
     for reno, recode in enumerate(recodes, start=1):
         for i, field in enumerate(fields):
             if i == 0:
-                del_flag = b'\x42' if recode[i] else b'\x32'
+                del_flag = b'\x2A' if recode[i] else b'\x20'
                 f.write(del_flag)
                 continue
-            if len(recode[i]) > field.length:
-                raise "记录{3:d}长度：{1:d}大于字段{0}设定长度：{2:d}，内容：{4}".format(
-                    field.name, len(recode[i]), field.length, reno, recode[i])
-            if reno == 1 and i == 5 and is_sse:
-                value = str(recode[i])[:field.length].ljust(field.length, ' ').encode()
+            if reno == 1 and i == 6 and is_sse:
+                value = str(recode[i])[:field.length].ljust(field.length, ' ').encode("gbk")
             elif field.type == 'N' or field.type == 'F':
-                value = str(recode[i]).rjust(field.length, ' ').encode()
+                if len(str(recode[i])) == 0 or recode[i] is None:
+                    value = "".join(['-' if field.length > 0 else "",
+                                     '.' + '-' * field.dec if field.length > 0 and field.dec > 0 else ""]
+                                    ).rjust(field.length, ' ').encode("gbk")
+                else:
+                    try:
+                        # rec = Decimal(recode[i]).quantize(Decimal("{0:.{1}f}".format(1.0, field.dec)))
+                        rec = "{0: >{1}.{2}f}".format(float(recode[i]), field.length, field.dec)
+                    except Exception as e:
+                        print("{0}，内容：{1},dec:{2}".format(repr(e), recode[i], field.dec))
+                    if len(rec) > field.length:
+                        rec = rec[0:field.length]
+                    value = rec.encode("gbk")  # .rjust(field.length, ' ')
             elif field.type == 'D':
-                value = recode[i].strftime('%Y%m%d').encode()
+                value = recode[i].strftime('%Y%m%d').encode("gbk")
             elif field.type == 'L':
-                value = str(recode[i])[0].upper().encode()
+                value = str(recode[i])[0].upper().encode("gbk")
             else:
-                value = str(recode[i])[:field.length].ljust(field.length, ' ').encode()
+                if len(str(recode[i])) > field.length:
+                    raise Exception("记录{3:d}长度：{1:d}大于字段{0}设定长度：{2:d}，内容：{4}".format(
+                        field.name, len(str(recode[i])), field.length, reno, recode[i]))
+                value = str(recode[i]).encode("gbk")[:field.length]
+                value += b' ' * (field.length - len(value))
             f.write(value)
 
 
@@ -53,7 +68,7 @@ def head_to_stream(f, fields, recnum):
     now = datetime.datetime.now()
     yr, mon, day = [now.year - 1900, now.month, now.day]
     numfields = len(fields)
-    lenheader = numfields * 32 + 33
+    lenheader = numfields * 32 + 1
     lenrecord = sum(field.length for field in fields)
     hdr = struct.pack(HEAD_FORMAT, ver, yr, mon, day, recnum, lenheader, lenrecord)
     f.write(hdr)
@@ -62,8 +77,8 @@ def head_to_stream(f, fields, recnum):
     for field in fields:
         if field.name == "del_flag":
             continue
-        name = field.name.ljust(11, '\x00').encode()
-        fld = struct.pack(FIELD_FORMAT, name, field.type[0].encode(), field.length, field.dec)
+        name = field.name.ljust(11, '\x00').encode("gbk")
+        fld = struct.pack(FIELD_FORMAT, name, field.type[0].encode("gbk"), field.length, field.dec)
         f.write(fld)
 
     # 结束文件头
@@ -74,25 +89,25 @@ class DbfSseWriter(object):
     def __init__(self, field_list=None):
         self.stream = io.BytesIO()
         if field_list:
-            self.fields = [FIELD(name, Type, length, dec) for [name, Type, length, dec] in field_list]
-        if self.fields:
-            self.fields.insert(0, FIELD('del_flag', 'C', 1, 0))
+            self._fields = [FIELD(name, Type, length, dec) for [name, Type, length, dec] in field_list]
+        if self._fields:
+            self._fields.insert(0, FIELD('del_flag', 'C', 1, 0))
         else:
-            self.fields = [FIELD('del_flag', 'C', 1, 0)]
+            self._fields = [FIELD('del_flag', 'C', 1, 0)]
 
     def write_to_stream(self, recodes):
         recnum = len(recodes)
         if not recnum:
             return False
-        head_to_stream(self.stream, self.fields, recnum)
-        recode_to_stream(self.stream, self.fields, recodes)
+        head_to_stream(self.stream, self._fields, recnum)
+        recode_to_stream(self.stream, self._fields, recodes)
         return True
 
     def stream_to_file(self, file_stream=None):
         if not file_stream:
             return False
         self.stream.seek(0, 2)
-        stream_len = len(self.stream.tell())
+        stream_len = self.stream.tell()
         file_stream.truncate(stream_len)
         file_stream.seek(0)
         file_stream.write(self.stream.getvalue())
@@ -100,7 +115,7 @@ class DbfSseWriter(object):
 
     @property
     def fields(self):
-        return self.fields
+        return self._fields
 
     @fields.setter
     def fields(self, value):
@@ -108,8 +123,8 @@ class DbfSseWriter(object):
         :type value: list of field
         filed sample FIELD(name,type,length,dec)
         """
-        assert isinstance(value, list) and len(value) > 0,'value is a list of FIELD'
+        assert isinstance(value, list) and len(value) > 0, 'value is a list of FIELD'
         if value[0].name != 'del_flag':
-            self.fields = [FIELD('del_flag', 'C', 1, 0)] + value
+            self._fields = [FIELD('del_flag', 'C', 1, 0)] + value
         else:
-            self.fields = value
+            self._fields = value
--- test.py
+++ test.py
@@ -11,7 +11,8 @@
 @time: 2017/2/2 13:55
 """
 import dbfclass
-from time import clock
+from time import clock, sleep
+from random import randint
 from datetime import datetime
 from decimal import Decimal
 
@@ -24,9 +25,12 @@ class Fast2Show:
         self.dataMap = {}
         self.mtk_file = r'd:\mktdt00.txt'
         self.fjy_path = r'd:\fjy'
-        self.dbf_file = r'd:\show203.dbf'
+        self.dbf_file = r'd:\show2003.dbf'
         self.isClose = False
         self.jyDate = ""
+        self.mkt_time = 0
+        self.fjy_time = 0
+        self.dbf_time = 0
 
     def read_file(self, file_stream):
         line = file_stream.readline()
@@ -34,7 +38,7 @@ class Fast2Show:
             yield line
             line = file_stream.readline()
 
-    def recode_strip(self, recodes):
+    def record_strip(self, recodes):
         for recode in recodes:
             re = [value.strip() for value in recode]
             yield re
@@ -44,10 +48,21 @@ class Fast2Show:
         mkt转show2003
         :return: True or False
         """
+        start = clock()
         first_line = second_line = third_line = ""
-        with open(self.mtk_file) as f:
-            lines = [x.replace('\n', '').split('|') for x in f]
-        lines = [x for x in recode_strip(lines)]
+        i = 0
+        while True:
+            try:
+                with open(self.mtk_file) as f:
+                    lines = [x.replace('\n', '').split('|') for x in f]
+                break
+            except IOError as e:
+                print(e.strerror)
+                i += 1
+                sleep(randint(1, 100) / 100.00)
+                if i > 3:
+                    return False
+        lines = [x for x in self.record_strip(lines)]
         for i, line in enumerate(lines):
             if i == 0:
                 first_line = line
@@ -57,10 +72,12 @@ class Fast2Show:
                 third_line = line
             else:
                 if i == 3:
-                    self.dataMap['000000'] = set_first_recode(first_line, second_line[9], third_line[9], line[9])
-                    mkt_to_map(second_line)
-                    mkt_to_map(third_line)
-                mkt_to_map(line)
+                    self.dataMap['000000'] = self.set_first_recode(first_line, second_line[9], third_line[9], line[9])
+                    self.mkt_to_map(second_line)
+                    self.mkt_to_map(third_line)
+                self.mkt_to_map(line)
+        self.mkt_time = clock() - start
+        return True
 
     def set_first_recode(self, line, zs_price, ag_price, bg_price):
         objs = [""] * 30
@@ -69,8 +86,8 @@ class Fast2Show:
         objs[2] = Decimal(ag_price)
         objs[3] = Decimal(bg_price)
         objs[4] = 0
-        jyDate = line[6][0:8]
-        objs[5] = jyDate
+        self.jyDate = line[6][0:8]
+        objs[5] = self.jyDate
         if line[8][0] == "E":
             objs[10] = 1111111111
             self.isClose = True
@@ -96,7 +113,7 @@ class Fast2Show:
                           else Decimal(records[4]).quantize(Decimal('1')))
             objs[5] = Decimal(records[7])
             objs[6] = Decimal(records[8])
-            objs[7] = Decimal(records[10] if isClose else records[9])
+            objs[7] = Decimal(records[10] if self.isClose else records[9])
             objs[10] = int(records[3])
             objs[11] = True
         else:
@@ -108,7 +125,7 @@ class Fast2Show:
                           else Decimal(records[4]).quantize(Decimal('1')))
             objs[5] = Decimal(records[7])
             objs[6] = Decimal(records[8])
-            objs[7] = Decimal(records[10] if isClose else records[9])
+            objs[7] = Decimal(records[10] if self.isClose else records[9])
             objs[8] = Decimal(records[11])
             objs[9] = Decimal(records[13])
             objs[10] = int(records[3])
@@ -143,14 +160,26 @@ class Fast2Show:
         self.dataMap[records[1]] = objs
 
     def write_fjy_to_stream(self):
+        start = clock()
         now = datetime.now()
         name = '{0}{1:0>2}{2:0>2}.txt'.format(now.year, now.month, now.day)
         fjy_file = self.fjy_path + name
-        with open(fjy_file) as f:
-            lines = [x.replace('\n', '').split('|') for x in f]
-        lines = [x for x in recode_strip(lines)]
+        i = 0
+        while True:
+            try:
+                with open(fjy_file) as f:
+                    lines = [x.replace('\n', '').split('|') for x in f]
+                break
+            except Exception as e:
+                print(repr(e))
+                i += 1
+                sleep(randint(1, 100) / 100.00)
+                if i > 3:
+                    return False
+
+        lines = [x for x in self.record_strip(lines)]
         for line in lines:
-            fjy_to_map(line)
+            self.fjy_to_map(line)
         self.dataMap["888880"] = [True, "888880", "新标准券", '1.0', '0.0', '0',
                                   '0.0', '0.0', '0.0', '0.0', '0.0', '0', "",
                                   '0', '0.0', '0', '0.0', '0', '0', '0.0', '0',
@@ -161,6 +190,8 @@ class Fast2Show:
                                   '0', '0.0', '0', '0.0', '0', '0', '0.0', '0',
                                   '0.0', '0', '0.0', '0', '0.0', '0', '0.0',
                                   '0', '0.0', '0']
+        self.fjy_time = clock() - start
+        return True
 
     def fjy_to_map(self, records):
         if len(records) == 0:
@@ -170,41 +201,41 @@ class Fast2Show:
                 '0', '0.0', '0', '0.0', '0', '0', '0.0', '0',
                 '0.0', '0', '0.0', '0', '0.0', '0', '0.0',
                 '0', '0.0', '0']
-        type = records[5]
-        id = records[1]
-        objs[0] = id
+        my_type = records[5]
+        my_id = records[1]
+        objs[0] = my_id
         objs[1] = records[2]
         if records[6] != "" and self.jyDate < records[6]:
             objs[11] = True
         elif records[7] != "" and self.jyDate > records[7]:
             objs[11] = True
 
-        if type in ("IS", "IN", "FS", "FC", "CR"):
+        if my_type in ("IS", "IN", "FS", "FC", "CR"):
             objs[2] = Decimal(records[11])
-        elif type in ("PH", "KK", "HK"):
+        elif my_type in ("PH", "KK", "HK"):
             objs[2] = Decimal(records[11])
             objs[11] = True
-        elif type in ("R1", "R2", "R3", "R4"):
+        elif my_type in ("R1", "R2", "R3", "R4"):
             objs[2] = objs[3] = Decimal(records[11])
-        elif type == "CV":
+        elif my_type == "CV":
             objs[2] = "100.000"
-        elif type in ("OC", "OR"):
+        elif my_type in ("OC", "OR"):
             objs[2] = Decimal(records[24])
             objs[7] = Decimal(records[25])
-        elif type in ("OS", "BD", "BW"):
+        elif my_type in ("OS", "BD", "BW"):
             objs[2] = "1.000"
-        elif type in ("OT", "OD", "OV"):
+        elif my_type in ("OT", "OD", "OV"):
             objs[2] = "0.000"
-        elif type in ("EC", "ER"):
+        elif my_type in ("EC", "ER"):
             t1 = self.T1IOPVMap[records[3]]
             if not t1:
                 objs[2] = Decimal(t1)
             t1 = self.IOPVMap[records[3]]
             if not t1:
                 objs[7] = Decimal(t1)
-        elif type == "EZ":
+        elif my_type == "EZ":
             objs[11] = True
-        elif id in ("799988", "799996", "799998", "799999", "939988"):
+        elif my_id in ("799988", "799996", "799998", "799999", "939988"):
             objs[2] = "1.000"
         if objs[11]:
             objs.insert(0, True)
@@ -212,3 +243,52 @@ class Fast2Show:
             objs.insert(0, False)
         objs[12] = ""
         self.dataMap[records[1]] = objs
+
+    def write_dbf(self):
+        start = clock()
+        keys = self.dataMap.keys()
+        fileds_list = [["S1", 'C', 6, 0], ["S2", 'C', 8, 0], ["S3", 'N', 8, 3],
+                       ["S4", 'N', 8, 3], ["S5", 'N', 12, 0], ["S6", 'N', 8, 3],
+                       ["S7", 'N', 8, 3], ["S8", 'N', 8, 3], ["S9", 'N', 8, 3],
+                       ["S10", 'N', 8, 3], ["S11", 'N', 10, 0], ["S13", 'N', 8, 3],
+                       ["S15", 'N', 10, 0], ["S16", 'N', 8, 3], ["S17", 'N', 10, 0],
+                       ["S18", 'N', 8, 3], ["S19", 'N', 10, 0], ["S21", 'N', 10, 0],
+                       ["S22", 'N', 8, 3], ["S23", 'N', 10, 0], ["S24", 'N', 8, 3],
+                       ["S25", 'N', 10, 0], ["S26", 'N', 8, 3], ["S27", 'N', 10, 0],
+                       ["S28", 'N', 8, 3], ["S29", 'N', 10, 0], ["S30", 'N', 8, 3],
+                       ["S31", 'N', 10, 0], ["S32", 'N', 8, 3], ["S33", 'N', 10, 0]]
+        writer = dbfclass.DbfSseWriter(fileds_list)
+        recodes = [self.dataMap[key] for key in sorted(keys)]
+        writer.write_to_stream(recodes)
+        i = 0
+        while True:
+            try:
+                with open(self.dbf_file, 'wb') as f:
+                    writer.stream_to_file(f)
+                break
+            except IOError:
+                print(IOError.strerror)
+                i += 1
+                sleep(randint(1, 100) / 100.00)
+                if i > 3:
+                    return False
+        self.T1IOPVMap.clear()
+        self.IOPVMap.clear()
+        self.dbf_time = clock() - start
+
+        return True
+
+
+if __name__ == '__main__':
+    samp = Fast2Show()
+    if samp.write_mkt_to_show():
+        if samp.write_fjy_to_stream():
+            if samp.write_dbf():
+                print("运行正确")
+                print("Mtk转Map时间：{0}，fjy转Map时间：{1}，Map写盘时间：{2}".format(samp.mkt_time, samp.fjy_time, samp.dbf_time))
+            else:
+                print("写文件错误")
+        else:
+            print("读fjy错误")
+    else:
+        print("读mkt错误")
