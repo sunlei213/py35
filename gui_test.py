e07c4a5c4d1ea8be3667d6f8c1535dd8fd6b353f
sunlei213 <sunleity@163.com>
2017-02-09 21:09:26 +0800

    python版dbf读写

--- gui_test.py
+++ gui_test.py
@@ -13,11 +13,14 @@
 
 from time import sleep
 import tkinter as tk
-from  threading import *
 from multiprocessing import *
+from queue import Empty
+import fast2dbf
+from datetime import datetime
+
 
 class MainWindow:
-    def __init__(self, title='nms', width=300, height=120, staFunc=bool, stoFunc=bool):
+    def __init__(self, title='nms', width=500, height=120, staFunc=bool, stoFunc=bool, que1=None, que2=None):
         self.w = width
         self.h = height
         self.stat = True
@@ -25,6 +28,9 @@ class MainWindow:
         self.stoFunc = stoFunc
         self.staIco = None
         self.stoIco = None
+        self.in_queue = que1
+        self.out_queue = que2
+
 
         self.root = tk.Tk(className=title)
 
@@ -36,7 +42,7 @@ class MainWindow:
         self.root.geometry('{}x{}+{}+{}'.format(self.w, self.h, x, y))
 
     def packBtn(self):
-        self.labl = tk.Label(self.root, fg='blue',text='显示', width=50, height=2)
+        self.labl = tk.Label(self.root, fg='blue',text='显示', width=80, height=2)
         self.labl.pack()
         self.btnSer = tk.Button(self.root, command=self.event, width=15, height=3)
         self.btnSer.pack(padx=20, side='left')
@@ -46,51 +52,87 @@ class MainWindow:
     def event(self):
         self.btnSer['state'] = 'disabled'
         if self.stat:
-            if self.stoFunc(self):
+            if self.stoFunc(self, self.in_queue, self.out_queue):
                 self.btnSer['text'] = '启动服务'
                 self.stat = False
                 self.root.iconbitmap(self.stoIco)
         else:
-            if self.staFunc(self):
+            if self.staFunc(self, self.in_queue, self.out_queue):
                 self.btnSer['text'] = '停止服务'
                 self.stat = True
                 self.root.iconbitmap(self.staIco)
         self.btnSer['state'] = 'active'
+
+    def _check_pipo(self):
+
+        try:
+            msg = self.in_queue.get_nowait()
+            self.labl['text'] = "loop_{0[3]}:Mtk时间：{0[0]:6.4f}，fjy时间：{0[1]:6.4f}，" \
+                                "写盘时间：{0[2]:6.4f}, 延迟{0[4]:.4f}秒".format(msg)
+        except Empty:
+            pass
+        self.root.after(500, self._check_pipo)
+
+
 
     def loop(self):
         self.root.resizable(False, False)   #禁止修改窗口大小
         self.packBtn()
         self.center()                       #窗口居中
         self.event()
+        self.root.after(500,self._check_pipo)
         self.root.mainloop()
 
+
 ########################################################################
-def run_pro(cls1):
-    global run_stat
+def run_pro(que1, que2):
+    run_stat = True
+    samp = fast2dbf.Fast2Show()
     i = 0
     while run_stat:
-        cls1.labl['text'] = '循环次数：{}'.format(i)
-        i += 1
-        sleep(3)
+        if samp.write_mkt_to_show():
+            if samp.write_fjy_to_stream():
+                if samp.write_dbf():
+                    i += 1
+                    print("loop_{3}:Mtk转Map时间：{0:6.4f}，fjy转Map时间：{1:6.4f}，Map写盘时间：{2:6.4f}"
+                          .format(samp.mkt_time, samp.fjy_time, samp.dbf_time, i))
+                else:
+                    print("写文件错误")
+            else:
+                print("读fjy错误")
+        else:
+            print("读mkt错误")
+        tm = 5.0 - samp.mkt_time - samp.fjy_time - samp.dbf_time
+        if tm < 0:
+            tm = 0.1
+        print("延迟{0:.4f}秒".format(tm))
+        que1.put([samp.mkt_time, samp.fjy_time, samp.dbf_time, i, tm])
+        try:
+            run_stat = que2.get_nowait()
+        except Empty:
+            pass
+        sleep(tm)
 
-def sta(cls1):
-    global run_stat
+def sta(cls1, que1, que2):
     cls1.labl['text'] = '开始'
-    run_stat = True
-    mp = Process(target=run_pro, args=(cls1,))
+    try:
+        que2.get_nowait()
+    except Empty:
+        pass
+    mp = Process(target=run_pro, args=(que1, que2))
     mp.start()
     return True
-def sto(cls1):
-    global run_stat
+
+def sto(cls1,  que1, que2):
     cls1.labl['text'] = '停止123456789012345678901234567890'
-    run_stat =  False
+    que2.put(False)
+    print('stop')
     return True
 
 if __name__ == '__main__':
     import sys, os
 
-    run_stat = True
-    w = MainWindow(staFunc=sta, stoFunc=sto)
+    w = MainWindow(staFunc=sta, stoFunc=sto, que1=Queue(), que2=Queue())
     w.staIco = os.path.join(sys.exec_prefix, 'DLLs\pyc.ico')
     w.stoIco = os.path.join(sys.exec_prefix, 'DLLs\py.ico')
     w.loop()
