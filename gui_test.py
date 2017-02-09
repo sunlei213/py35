15a852ef4e9d5f28f59807fdfdf25ba09a8a605e
sunlei213 <sunleity@163.com>
2017-02-09 21:18:44 +0800

    python版dbf读写

--- gui_test.py
+++ gui_test.py
@@ -112,6 +112,27 @@ def run_pro(que1, que2):
         except Empty:
             pass
         sleep(tm)
+    if not run_stat:
+        i = 0
+        que2.close()
+    while i < 20:
+        if samp.write_mkt_to_show():
+            if samp.write_fjy_to_stream():
+                if samp.write_dbf():
+                    i += 1
+                    print("loop_{3}:Mtk转Map时间：{0:6.4f}，fjy转Map时间：{1:6.4f}，Map写盘时间：{2:6.4f}"
+                          .format(samp.mkt_time, samp.fjy_time, samp.dbf_time, i))
+                else:
+                    print("写文件错误")
+            else:
+                print("读fjy错误")
+        else:
+            print("读mkt错误")
+        tm = 5.0 - samp.mkt_time - samp.fjy_time - samp.dbf_time
+        if tm < 0:
+            tm = 0.1
+        print("延迟{0:.4f}秒".format(tm))
+        sleep(tm)
 
 def sta(cls1, que1, que2):
     cls1.labl['text'] = '开始'
